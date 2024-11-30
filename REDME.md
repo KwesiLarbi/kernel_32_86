957a28464b9595e72778388eb80cb9161809bf15
KwesiLarbi <KwesiiiLarb@gmail.com>
2024-11-30 00:53:39 -0500

    creating kernel output

--- REDME.md
+++ REDME.md
@@ -1,1 +1,9 @@
 # Simple Kernel for 32-bit x86
+
+# Compile the kernel file
+
+`
+export PREFIX="$HOME/opt/cross/install"
+export TARGET=i686-elf
+$PREFIX/bin/$TARGET-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+`
--- kernel.c
+++ kernel.c
@@ -106,4 +106,8 @@ void terminal_writestring(const char* data)
 void kernel_main(void)
 {
     /* Initialize terminal interface */
+    terminal_initialize();
+
+    /* Newline support is left as an exercise */
+    terminal_writestring("Hello, Kernel World!\n");
 }
